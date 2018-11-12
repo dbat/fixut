79273e9ca314df99661cfec8e5d16118458c7c54
dadang <aadbox-5@yahoo.com>
2018-11-12 17:11:37 +0700

    add option -d

--- fixuta.cmd
+++ fixuta.cmd
@@ -5,28 +5,50 @@ goto START
 
 :Help
 echo.
-echo. SYNOPSYS
-echo.	fix all broken torrent files (*.!ut) in current dir
+echo. SYNOPSYS:
+echo.	fix all broken torrent files (*.^^!ut) in current dir
+echo.
+echo. USAGE:
+echo. 	%~n0 [ -d ]
+echo. 	-d or /d: (optional) delete *.^^!ut files after fix
 echo.
 echo. REQUIRES:
 echo.	fixut.exe (get it from https://github.com/dbat/fixut)
+echo.
+echo. NOTES:
+echo. 	Windows XP+
 echo.
 
 exit/b
 
 :START
-if /i "%~1"=="-h" goto Help
 if /i "%~1"=="/?" goto Help
+if /i "%~1"=="-?" goto Help
+if /i "%~1"=="-h" goto Help
+if /i "%~1"=="--help" goto Help
 if not exist %fixut% goto Help
 
-for %%f in (*.?ut) do (
+set "DELUT="
+if /i "%~1"=="/d" set "DELUT=1"
+if /i "%~1"=="-d" set "DELUT=1"
+
+set "CTR=0"
+for %%f in (*.^^!ut) do (
   if exist "%%~nf" (
     %fixut% "%%~nf"
+    if "%DELUT%"=="1" del "%%~nf.^!ut"
+    set /a "CTR=!CTR!+1"
   ) else (
     echo. Warning^^! Missing file: "%%~nf"
   )
 )
 echo.
-echo. All done, you can delete all succesfully repaired *ut files
-
-@pause
+set "FILES1=files"
+if "%CTR%"=="1" set "FILES1=file"
+if %CTR% gtr 0 (
+  echo. All done, %CTR% %FILES1% processed
+  if not "%DELUT%"=="1" echo. You can delete all succesfully repaired *.^^!ut files
+) else (
+  echo. No matching files
+)
+pause
